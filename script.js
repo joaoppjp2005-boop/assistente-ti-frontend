555dfcfafa83e6b822ece9ce752fd0d5c113aba0
joaoppjp2005-boop <joaoppjp2005@gmail.com>
2026-09-19 22:45:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // URL do teu backend no Render
-const BACKEND_URL = "hhttps://assistente-ti-backendd.onrender.com/";
+const BACKEND_URL = "https://assistente-ti-backend.onrender.com/chat";
 
 const chatBox = document.getElementById("chat-box");
 const userInput = document.getElementById("user-input");
