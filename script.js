d07eb6793c69680af81e0772760181b17981e154
joaoppjp2005-boop <joaoppjp2005@gmail.com>
2026-09-21 18:24:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
-// 2. Configuração do Firebase
+// 2. Configuração das credenciais
 const firebaseConfig = {
-  apiKey: "AIzaSyB_r5EcyShSoypck4A1fPxlhod6-HDgIPk",
-  authDomain: "chronical-81382.firebaseapp.com",
-  projectId: "chronical-81382",
-  storageBucket: "chronical-81382.firebasestorage.app",
-  messagingSenderId: "657212411165",
-  appId: "1:657212411165:web:760b7380c4a3dd903f25f4",
-  measurementId: "G-5ZYH88PHEE"
+  apiKey: "AIzaSyAq1MaAD_jph9cVDDuYbCq1QxwKqLv64RE",
+  authDomain: "chronical-624a2.firebaseapp.com",
+  projectId: "chronical-624a2",
+  storageBucket: "chronical-624a2.firebasestorage.app",
+  messagingSenderId: "919413941674",
+  appId: "1:919413941674:web:a2e56297e56f666b44f7d3",
+  measurementId: "G-KP1PZPM94N"
 };
 
 // 3. Inicializa o Firebase
@@ -22,7 +22,7 @@ const provider = new GoogleAuthProvider();
 export function fazerLoginComGoogle() {
   signInWithPopup(auth, provider)
     .then((result) => {
-      console.log("Login com sucesso:", result.user);
+      console.log("Login realizado com sucesso:", result.user);
       alert(`Bem-vindo(a), ${result.user.displayName}!`);
     })
     .catch((error) => {
@@ -31,7 +31,7 @@ export function fazerLoginComGoogle() {
     });
 }
 
-// 5. Associa ao botão
+// 5. Conecta ao botão quando a página carregar
 document.addEventListener("DOMContentLoaded", () => {
   const btnGoogle = document.getElementById("btn-google");
   if (btnGoogle) {
